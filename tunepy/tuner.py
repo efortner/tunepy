b11041d4e7c5faff20253e193b4c743b6746fe0c
Ethan Fortner <efortner@indeed.com>
2020-01-23 22:05:47 -0600

    Drafted Tuner class and its builder

--- tunepy/tuner.py
+++ tunepy/tuner.py
@@ -1,4 +1,32 @@
 
 
 class Tuner:
-    pass
+    """
+    A generic supervised learning hyperparameter tuner.
+    """
+
+    class Builder:
+        """
+        Builds a new customized instance of Tuner.
+        """
+
+        def __init__(self, x, y):
+            """
+            Creates a new Tuner.Builder.
+            :param x: Array-like data set features.
+            :param y: Vector of data set labels.
+            """
+            self.bundle = {
+                'x': x,
+                'y': y,
+            }
+
+        def build(self):
+            """
+            Creates a new instance of Tuner container the properties specified by the builder.
+            :return: A new instance of Tuner.
+            """
+            return Tuner(self.bundle)
+
+    def __init__(self, bundle):
+        pass
