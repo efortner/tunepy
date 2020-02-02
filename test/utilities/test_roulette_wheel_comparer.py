2470957c514ef8394764902c3bec5e99e7557f92
Ethan Fortner <efortner@indeed.com>
2020-02-02 17:06:56 -0600

    Roulette wheel comparer algorithm not working correctly

--- test/utilities/test_roulette_wheel_comparer.py
+++ test/utilities/test_roulette_wheel_comparer.py
@@ -8,12 +8,49 @@ class TestRouletteWheelComparer(unittest.TestCase):
     def test_fitness_extraction(self):
         rng = NumpyOnesRandom()
         comparer = RouletteWheelComparer(rng)
-        models = [ConstantFitnessLearner(10-index) for index in range(10)]
-        fitnesses = comparer._extract_fitness_from_models(models)
+        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
+        comparer._extract_fitness_from_models(models)
+        fitnesses = comparer._all_fitness
 
-        self.assertEqual(10, len(models))
+        self.assertEqual(10, len(fitnesses))
         self.assertEqual(1, fitnesses[0])
         self.assertEqual(10, fitnesses[-1])
+
+    def test_multiple_fitness_extraction(self):
+        rng = NumpyOnesRandom()
+        comparer = RouletteWheelComparer(rng)
+        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
+
+        first_extraction = comparer._extract_fitness_from_models(models)
+        second_extraction = comparer._extract_fitness_from_models(models)
+
+        self.assertTrue(first_extraction)
+        self.assertFalse(second_extraction)
+
+    def test_returns_best_model(self):
+        rng = NumpyOnesRandom()
+        comparer = RouletteWheelComparer(rng)
+        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
+
+        returned_model = comparer.compare(models)
+        self.assertEqual(10, returned_model.fitness)
+
+    def test_returns_worst_model(self):
+        rng = NumpyPassThroughRandom(0.0)
+        comparer = RouletteWheelComparer(rng)
+        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
+
+        returned_model = comparer.compare(models)
+        self.assertEqual(1, returned_model.fitness)
+
+    def test_returns_median_model(self):
+        rng = NumpyPassThroughRandom(0.5)
+        comparer = RouletteWheelComparer(rng)
+        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
+
+        returned_model = comparer.compare(models)
+        self.assertEqual(5, returned_model.fitness)
+
 
 if __name__ == '__main__':
     unittest.main()
--- tunepy/comparers/roulette_wheel_comparer.py
+++ tunepy/comparers/roulette_wheel_comparer.py
@@ -4,8 +4,57 @@ import numpy as np
 
 class RouletteWheelComparer(AbstractModelComparer):
     """
-    Performs a fitness proportionate random model selection.
+    Performs a fitness proportionate random model selection. This is a common method for selecting breeding pairs
+    for a genetic algorithm.
     """
 
+    def __init__(self, random_number_generator):
+        """
+        Creates a new RouletteWheelComparer.
+        :param random_number_generator:
+        """
+        self._rng = random_number_generator
+        self._hash_models = 0
+        self._all_fitness = None
+        self._max_fitness = 0.0
+        self._fitness_to_model = {}
+
     def compare(self, models):
-        pass
+        """
+        Returns one model for the provided list of models chosen by roulette wheel style selection.
+        :param models: Array-like of objects implementing AbstractLearner.
+        :return: One model object from models.
+        """
+
+        def recursive_fitness_search(all_fitnesses, search_fitness):
+            if len(all_fitnesses) == 1:
+                return all_fitnesses[0]
+
+            middle_index = int(len(all_fitnesses) / 2)
+            if all_fitnesses[middle_index] > search_fitness:
+                return recursive_fitness_search(all_fitnesses[:middle_index], search_fitness)
+            else:
+                return recursive_fitness_search(all_fitnesses[middle_index:], search_fitness)
+
+        self._extract_fitness_from_models(models)
+        target_fitness = self._rng.random() * self._max_fitness
+        found_fitness = recursive_fitness_search(self._all_fitness, target_fitness)
+
+        return self._fitness_to_model[float(found_fitness)]
+
+    def _extract_fitness_from_models(self, models):
+        new_hash_models = hash(tuple(models))
+        if new_hash_models == self._hash_models:
+            return False
+
+        self._hash_models = new_hash_models
+        self._all_fitness = np.zeros(shape=(len(models),))
+
+        for index in range(len(models)):
+            current_fitness = float(models[index].fitness)
+            self._all_fitness[index] = current_fitness
+            self._fitness_to_model[current_fitness] = models[index]
+
+        self._all_fitness = np.sort(self._all_fitness)
+        self._max_fitness = np.max(self._all_fitness)
+        return True
