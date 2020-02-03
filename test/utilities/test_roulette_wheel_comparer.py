b11c312351be94b2661825fcde32d4dd2fea6c04
Ethan Fortner <efortner@indeed.com>
2020-02-02 18:10:01 -0600

    Roulette wheel working all tests passing

--- test/utilities/test_roulette_wheel_comparer.py
+++ test/utilities/test_roulette_wheel_comparer.py
@@ -1,34 +1,21 @@
 import unittest
 from tunepy.comparers import RouletteWheelComparer
-from tunepy.interfaces.stubs import NumpyPassThroughRandom, NumpyOnesRandom, ConstantFitnessLearner
+from tunepy.interfaces import AbstractRandomNumberGenerator
+from tunepy.interfaces.stubs import NumpyOnesRandom, ConstantFitnessLearner
 
 
 class TestRouletteWheelComparer(unittest.TestCase):
 
-    def test_fitness_extraction(self):
-        rng = NumpyOnesRandom()
-        comparer = RouletteWheelComparer(rng)
-        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
-        comparer._extract_fitness_from_models(models)
-        fitnesses = comparer._all_fitness
-
-        self.assertEqual(10, len(fitnesses))
-        self.assertEqual(1, fitnesses[0])
-        self.assertEqual(10, fitnesses[-1])
-
-    def test_multiple_fitness_extraction(self):
-        rng = NumpyOnesRandom()
-        comparer = RouletteWheelComparer(rng)
-        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
-
-        first_extraction = comparer._extract_fitness_from_models(models)
-        second_extraction = comparer._extract_fitness_from_models(models)
-
-        self.assertTrue(first_extraction)
-        self.assertFalse(second_extraction)
-
     def test_returns_best_model(self):
-        rng = NumpyOnesRandom()
+        class CustomRandom(AbstractRandomNumberGenerator):
+
+            def random_int_array(self, minimum, maximum, shape):
+                return [0]
+
+            def random(self):
+                return 0.0
+
+        rng = CustomRandom()
         comparer = RouletteWheelComparer(rng)
         models = [ConstantFitnessLearner(10 - index) for index in range(10)]
 
@@ -36,7 +23,15 @@ class TestRouletteWheelComparer(unittest.TestCase):
         self.assertEqual(10, returned_model.fitness)
 
     def test_returns_worst_model(self):
-        rng = NumpyPassThroughRandom(0.0)
+        class CustomRandom(AbstractRandomNumberGenerator):
+
+            def random_int_array(self, minimum, maximum, shape):
+                return [9]
+
+            def random(self):
+                return 0.0
+
+        rng = CustomRandom()
         comparer = RouletteWheelComparer(rng)
         models = [ConstantFitnessLearner(10 - index) for index in range(10)]
 
@@ -44,13 +39,32 @@ class TestRouletteWheelComparer(unittest.TestCase):
         self.assertEqual(1, returned_model.fitness)
 
     def test_returns_median_model(self):
-        rng = NumpyPassThroughRandom(0.5)
+        class CustomRandom(AbstractRandomNumberGenerator):
+
+            def random_int_array(self, minimum, maximum, shape):
+                return [5]
+
+            def random(self):
+                return 0.0
+
+        rng = CustomRandom()
         comparer = RouletteWheelComparer(rng)
         models = [ConstantFitnessLearner(10 - index) for index in range(10)]
 
         returned_model = comparer.compare(models)
         self.assertEqual(5, returned_model.fitness)
 
+    def test_cached_fitness(self):
+        rng = NumpyOnesRandom()
+        comparer = RouletteWheelComparer(rng)
+        models = [ConstantFitnessLearner(10 - index) for index in range(10)]
+
+        first_check = comparer._extract_fitness_from_models(models)
+        second_check = comparer._extract_fitness_from_models(models)
+
+        self.assertTrue(first_check)
+        self.assertFalse(second_check)
+        self.assertEqual(10, comparer._max_fitness)
 
 if __name__ == '__main__':
     unittest.main()
--- tunepy/comparers/roulette_wheel_comparer.py
+++ tunepy/comparers/roulette_wheel_comparer.py
@@ -4,20 +4,20 @@ import numpy as np
 
 class RouletteWheelComparer(AbstractModelComparer):
     """
-    Performs a fitness proportionate random model selection. This is a common method for selecting breeding pairs
-    for a genetic algorithm.
+    Performs a fitness proportionate random model selection. The underlying implementation uses the stochastic
+    acceptance method for producing a result in nearly constant time. This is a common method for selecting
+    breeding pairs for a genetic algorithm.
     """
 
     def __init__(self, random_number_generator):
         """
         Creates a new RouletteWheelComparer.
-        :param random_number_generator:
+        :param random_number_generator: An object of a class that implements AbstractRandomNumberGenerator.
         """
         self._rng = random_number_generator
         self._hash_models = 0
         self._all_fitness = None
         self._max_fitness = 0.0
-        self._fitness_to_model = {}
 
     def compare(self, models):
         """
@@ -26,21 +26,13 @@ class RouletteWheelComparer(AbstractModelComparer):
         :return: One model object from models.
         """
 
-        def recursive_fitness_search(all_fitnesses, search_fitness):
-            if len(all_fitnesses) == 1:
-                return all_fitnesses[0]
-
-            middle_index = int(len(all_fitnesses) / 2)
-            if all_fitnesses[middle_index] > search_fitness:
-                return recursive_fitness_search(all_fitnesses[:middle_index], search_fitness)
-            else:
-                return recursive_fitness_search(all_fitnesses[middle_index:], search_fitness)
-
         self._extract_fitness_from_models(models)
-        target_fitness = self._rng.random() * self._max_fitness
-        found_fitness = recursive_fitness_search(self._all_fitness, target_fitness)
 
-        return self._fitness_to_model[float(found_fitness)]
+        while True:
+            chosen_model = models[self._rng.random_int(0, len(models))]
+            acceptance_probability = chosen_model.fitness / self._max_fitness
+            if acceptance_probability > self._rng.random():
+                return chosen_model
 
     def _extract_fitness_from_models(self, models):
         new_hash_models = hash(tuple(models))
@@ -51,10 +43,7 @@ class RouletteWheelComparer(AbstractModelComparer):
         self._all_fitness = np.zeros(shape=(len(models),))
 
         for index in range(len(models)):
-            current_fitness = float(models[index].fitness)
-            self._all_fitness[index] = current_fitness
-            self._fitness_to_model[current_fitness] = models[index]
+            self._all_fitness[index] = models[index].fitness
 
-        self._all_fitness = np.sort(self._all_fitness)
         self._max_fitness = np.max(self._all_fitness)
         return True
