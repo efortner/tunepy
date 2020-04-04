05f9e1c5bab06481b838ccfa83dc944f4a7d403c
Ethan Fortner <efortner@indeed.com>
2020-04-04 11:48:17 -0500

    Built and tested non-improvement convergence criterion

--- tunepy/convergence/__init__.py
+++ tunepy/convergence/__init__.py
@@ -1,2 +1,2 @@
-from tunepy.convergence.simple_convergence import *
+from tunepy.convergence.simple_convergence import Iterations, ConsecutiveNonImprovement
 from tunepy.convergence.annealing_schedules import *
--- tunepy/convergence/simple_convergence.py
+++ tunepy/convergence/simple_convergence.py
@@ -21,3 +21,38 @@ class Iterations(AbstractConvergenceCriterion):
 
         self._current_iteration += 1
         return False
+
+
+class ConsecutiveNonImprovement(AbstractConvergenceCriterion):
+    def __init__(self, n, epsilon):
+        self._last_iteration = n - 1
+        self._iteration_count = 0
+        self._epsilon = epsilon
+        self._net_improvement = 0.0
+
+    def converged(self, old_population, new_population):
+        if self._net_improvement < self._epsilon and self._iteration_count == self._last_iteration:
+            return True
+
+        old_fitness = get_best_fitness(old_population)
+        new_fitness = get_best_fitness(new_population)
+
+        if self._iteration_count == 0:
+            self._net_improvement = new_fitness - old_fitness
+        else:
+            self._net_improvement += new_fitness - old_fitness
+
+        if self._net_improvement < self._epsilon:
+            self._iteration_count += 1
+        else:
+            self._iteration_count = 0
+
+        return False
+
+
+def get_best_fitness(population):
+    best_fitness = float('-inf')
+    for genome in population:
+        if genome.fitness > best_fitness:
+            best_fitness = genome.fitness
+    return best_fitness
